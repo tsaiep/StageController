6ff625fddf11b0101925c118da4b943238bc41c3
Pei <brian19971014@gmail.com>
2026-07-05 16:17:32 +0800

    WIP

--- Assets/_FeelCue/Script/Timeline/SeededMMFVFXMixer.cs
+++ Assets/_FeelCue/Script/Timeline/SeededMMFVFXMixer.cs
@@ -16,11 +16,10 @@ public class SeededMMFVFXMixer : PlayableBehaviour
         double rootTime = playable.GetGraph().GetRootPlayable(0).GetTime();
         bool hasLastRootTime = !double.IsNaN(lastRootTime);
         double deltaTime = hasLastRootTime ? rootTime - lastRootTime : 0.0;
-        bool timelineRewound = hasLastRootTime && deltaTime < 0.0;
-        bool isForwardPlayback = info.evaluationType == FrameData.EvaluationType.Playback &&
-                                 hasLastRootTime &&
-                                 deltaTime > 0.0 &&
-                                 deltaTime <= SeekThreshold;
+        bool isSeekOrScrub = !hasLastRootTime ||
+                             deltaTime <= 0.0 ||
+                             deltaTime > SeekThreshold ||
+                             info.evaluationType != FrameData.EvaluationType.Playback;
 
         int inputCount = playable.GetInputCount();
         for (int i = 0; i < inputCount; i++)
@@ -34,28 +33,22 @@ public class SeededMMFVFXMixer : PlayableBehaviour
                                     (double.IsInfinity(duration) || localTime <= duration + TimeEpsilon);
             bool isActive = playable.GetInputWeight(i) > 0f && isInsideClipTime;
 
-            if (timelineRewound)
-            {
-                behaviour.triggered = false;
-                behaviour.wasActive = false;
-            }
-
             if (!isActive)
             {
+                if (deltaTime < 0.0)
+                    behaviour.triggered = false;
+
                 behaviour.wasActive = false;
                 continue;
             }
 
-            double clipStart = rootTime - localTime;
-            bool enteredClip = !behaviour.wasActive &&
-                               isForwardPlayback &&
-                               lastRootTime < clipStart &&
-                               rootTime >= clipStart &&
-                               localTime <= deltaTime + TimeEpsilon;
-
+            bool enteredClipDuringThisFrame = !behaviour.wasActive &&
+                                              !isSeekOrScrub &&
+                                              localTime >= -TimeEpsilon &&
+                                              localTime <= deltaTime + TimeEpsilon;
             behaviour.wasActive = true;
 
-            if (!enteredClip || behaviour.triggered)
+            if (!enteredClipDuringThisFrame || behaviour.triggered)
                 continue;
 
             behaviour.triggered = true;
